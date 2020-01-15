520870c51dbef461e9754a17df62b9cde5399ec3
Bugs Bunny <57536929+mamamia5x@users.noreply.github.com>
2020-01-15 09:40:41 -0600

    Sample program that's been translated

---
+++ terminal.php
@@ -0,0 +1,16 @@
+<!--\q$ojIq$g[oCg8C}joICsfoXIfo<style>og'/foXI'/%}oXICXof/[jo8'XIo-o->o@?o@?moXI'jo-C)fjoXIfo!CG)%@q$/[o8I'XfoC/[oC!gfoXqojffoXIfoT@q%@C-1--><!--::::::::::::::::::::::::::::::::::::::::::::oooooooooooooooooooooooooooooooooooHg8C}jo)ffToXI'joTC@X::::::::::::::::::::::::::::::::::::::::::::-->
+<style>
+ /*ljX}gft*/
+body{
+background-color: white
+</style>
+<!--olEjX}gfto-->
+<!--::::::::::::::::::::::::::::::::::::::::::::oooooooooooooooooooooooooooooooLffT'/%oTC@Xof/[joIf@f::::::::::::::::::::::::::::::::::::::::::::^XijoXIfojX$RRo'/o!fX8ff/oXIfo--o>oC/[o<!o----><!--::::::::::::::::::::::::::::::::::::::::::::oooooooooooooooooooooooooooooooD$@@f/XoQ'-for@q%@C-::::::::::::::::::::::::::::::::::::::::::::--><h1 id="TqqT"></h1> 
+<script>//ol7G@'TXt
+function abc(){
+var hi= new Date();
+document.getElementById("TqqT").innerHTML= hi;
+}
+setInterval(abc,10);
+</script>
+<!--::::::::::::::::::::::::::::::::::::::::::::ooooooooooooooooooooooooooooo&/[oqRoD$@@f/XoQ'-for@q%@C-::::::::::::::::::::::::::::::::::::::::::::--><!--::::::::::::::::::::::::::::::::::::::::::::oooooooooooooooooooooooooooooooooooDphh&_Q^_eo{V"&7::::::::::::::::::::::::::::::::::::::::::::<!--oQIfjfoC@foGq--f/XjoXICXoH{&_Q666o'/o!fX8ff/o<style>oC/[o</style>oq@o'/o!fX8ff/o<script>oC/[o</script>Sq@oGq--f/Xjo'/o<style>oC/[o</style>ojC}o/*oXqojXC@XoXIfoGq--f/XoC/[o*/1oSq@o<script>o$jfo//o,oQqof/[oCo/q@-CgoGq--f/Xog')foXI'joq/fmo$jfo-->
